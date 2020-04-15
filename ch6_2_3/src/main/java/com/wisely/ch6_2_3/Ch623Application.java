33535d08a18198f1eaf6a28d1adbb9b02779b47b
苏晓慧 <suxh@fadada.com>
2020-04-15 15:19:19 +0800

    整理

--- ch6_2_3/src/main/java/com/wisely/ch6_2_3/Ch623Application.java
+++ ch6_2_3/src/main/java/com/wisely/ch6_2_3/Ch623Application.java
@@ -3,12 +3,14 @@ package com.wisely.ch6_2_3;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.wisely.ch6_2_3.config.AuthorSettings;
 @RestController
 @SpringBootApplication
+@EnableConfigurationProperties
 public class Ch623Application {
 	
 	@Autowired
